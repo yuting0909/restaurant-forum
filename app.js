fca8aa53769591ace6925205a19ed2a1205a40ca
Browny <kyul4852@gmail.com>
2021-07-27 17:53:50 +0800

    Initialize project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const handlebars = require('express-handlebars')
 const port = 3000
+
+app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
