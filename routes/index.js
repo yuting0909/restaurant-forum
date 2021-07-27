2f60b5d251f36ffd3cd50cb4b322f892efb748b3
Browny <kyul4852@gmail.com>
2021-07-27 21:07:22 +0800

    add user & admin routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
-module.exports = (app) => {
+const restController = require('../controllers/restController')
+const adminController = require('../controllers/adminController')
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+module.exports = (app) => {
+  app.get('/', (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', restController.getRestaurants)
+
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', adminController.getRestaurants)
 }
