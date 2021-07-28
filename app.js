ba53523692c2ca012ee7a79d4ed9839529f7f446
Browny <kyul4852@gmail.com>
2021-07-28 16:40:12 +0800

    add feature: signin

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const db = require('./models')
 const port = 3000
 
@@ -12,6 +13,8 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
@@ -24,6 +27,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
