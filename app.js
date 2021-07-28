5732ec7abcca691e327643feb4bc5bf612c0c2c2
Browny <kyul4852@gmail.com>
2021-07-28 14:34:47 +0800

    add flash messages of different password and duplicate email in signup page

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
 const port = 3000
 
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   // db.sequelize.sync()
