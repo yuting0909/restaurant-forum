ac0e492dca6f8019d6f247cfca394ad6a9a67b99
Browny <kyul4852@gmail.com>
2021-07-29 12:13:36 +0800

    Listen to process.env.PORT & define Procfile

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session')
 const methodOverride = require('method-override')
 const passport = require('./config/passport')
 const db = require('./models')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use('/upload', express.static(__dirname + '/upload'))
 
