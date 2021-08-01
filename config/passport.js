72d8465302ff2aab789a7669aeebeab34713c452
Browny <kyul4852@gmail.com>
2021-08-02 02:24:29 +0800

    add authenticated and authenticatedAdmin

--- config/passport.js
+++ config/passport.js
@@ -40,4 +40,29 @@ passport.deserializeUser((id, cb) => {
   })
 })
 
+// JWT
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+require('dotenv').config()
+
+let jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+jwtOptions.secretOrKey = process.env.JWT_SECRET
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  User.findByPk(jwt_payload.id, {
+    include: [
+      { model: Restaurant, as: 'FavoritedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  }).then(user => {
+    if (!user) return next(null, false)
+    return next(null, user)
+  })
+})
+passport.use(strategy)
+
 module.exports = passport
--- routes/apis.js
+++ routes/apis.js
@@ -2,16 +2,27 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
-
+const passport = require('../config/passport')
 const adminController = require('../controllers/api/adminController.js')
 const userController = require('../controllers/api/userController.js')
 
-router.get('/admin/restaurants', adminController.getRestaurants)
-router.get('/admin/restaurants/:id', adminController.getRestaurant)
-router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
-router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
-router.get('/admin/categories', adminController.getCategories)
-router.get('/admin/categories/:id', adminController.getCategories)
+const authenticated = passport.authenticate('jwt', { session: false })
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) { return next() }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
+
+router.get('/admin/restaurants', authenticated, authenticatedAdmin, adminController.getRestaurants)
+router.get('/admin/restaurants/:id', authenticated, authenticatedAdmin, adminController.getRestaurant)
+router.post('/admin/restaurants', authenticated, authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
+router.delete('/admin/restaurants/:id', authenticated, authenticatedAdmin, adminController.deleteRestaurant)
+
+router.get('/admin/categories', authenticated, authenticatedAdmin, adminController.getCategories)
+router.get('/admin/categories/:id', authenticated, authenticatedAdmin, adminController.getCategories)
 
 router.post('/signin', userController.signIn)
 
